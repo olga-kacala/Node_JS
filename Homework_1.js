6bfe3cb1ce88b3b0fbc9f90cfc4cef1544632a54
olga-kacala <olga.kacala@gmail.com>
2024-04-03 19:31:01 +0200

    string functions

--- Homework_1.js
+++ Homework_1.js
@@ -8,42 +8,136 @@
 // For subtraction, ensure that the first parameter is always greater than the second parameter.
 // Division should only result in an integer value.
 
-let string1 = "99999999999999999999999999999999999999999999999999"; // string1 has to be > string2
-let string2 = "2";
+const str1 = "10"; // string1 has to be > string2
+const str2 = "2";
+
+function homeworkPlusMinus(string1, string2, operation) {
+  const longerStringLength =
+    string1.length > string2.length ? string1.length : string2.length;
+  const strings = [string1, string2].map((item) =>
+    item.padStart(longerStringLength, "0").split("").reverse()
+  );
+
+  const res = strings[0].reduce(
+    (acc, cur, ind) => {
+      const a = cur;
+      const b = strings[1][ind];
+      const c = acc.leftover;
+      const sum = [a, b, c].map(Number).reduce(operation, 0);
+      const newDigit = String(sum).at(-1);
+      const newDigits = [newDigit, ...acc.digits];
+      const newLeftover = String(sum).slice(0, -1);
+      return { digits: newDigits, leftover: newLeftover };
+    },
+    {
+      digits: [],
+      leftover: 0,
+    }
+  );
+
+  return res.leftover + res.digits.join("");
+}
 
 // String.plus(string): This function should take another string as input and return the result of adding the two strings together.
-function stringPlus(string1, string2) {
-  let sum = BigInt(string1) + BigInt(string2);
-  return sum.toString();
-}
-let summary = stringPlus(string1, string2);
-console.log(string1 + " + " + string2 + " = " + summary);
+
+const stringPlus = (a, c) => a + c;
 
 // String.minus(string): This function should take another string as input and return the result of subtracting the second string from the first string. Note that the first parameter will always be greater than the second parameter.
 
-function stringMinus(string1, string2) {
-  if (BigInt(string1) >= BigInt(string2)) {
-    let sub = BigInt(string1) - BigInt(string2);
-    return sub.toString();
-  } else {
-    return "Incorrect input. String1 must be greater than or equal to String2.";
+const stringMinus = (a, c, i) => {
+  if (i === 0) {
+    return c;
   }
-}
-let difference = stringMinus(string1, string2);
-console.log(string1 + " - " + string2 + " = " + difference);
+  return a - c;
+};
 
 //   String.divide(string): This function should take another string as input and return the result of dividing the first string by the second string. Division should only result in an integer value.
-function stringDivide(string1, string2) {
-  let div = BigInt(string1) / BigInt(string2);
-  return div.toString();
+
+function homeworkDivide(string1, string2) {
+  const num1 = string1.split("").map(Number);
+  const num2 = string2.split("").map(Number);
+  const len1 = num1.length;
+  const len2 = num2.length;
+
+  if (len1 < len2 || (len1 === len2 && string1 < string2)) {
+    return "0"; // If string1 < string2, division result will be 0
+  }
+
+  const result = [];
+  let dividend = num1.slice();
+
+  while (dividend.length) {
+    let quotientDigit = 0;
+    while (isGreaterOrEqual(dividend, num2)) {
+      quotientDigit++;
+      dividend = subtract(dividend, num2);
+    }
+    result.push(quotientDigit);
+    dividend.shift();
+  }
+
+  return result.join("").replace(/^0+/, "") || "0";
 }
-let divide = stringDivide(string1, string2);
-console.log(string1 + " / " + string2 + " = " + divide);
+
+function isGreaterOrEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return arr1.length > arr2.length;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return arr1[i] > arr2[i];
+    }
+  }
+  return true; // arr1 === arr2
+}
+
+function subtract(arr1, arr2) {
+  const result = [];
+  let borrow = 0;
+
+  for (let i = 0; i < arr1.length; i++) {
+    let diff = arr1[i] - (arr2[i] || 0) - borrow;
+    if (diff < 0) {
+      diff += 10;
+      borrow = 1;
+    } else {
+      borrow = 0;
+    }
+    result.push(diff);
+  }
+
+  return result;
+}
 
 // String.multiply(string): This function should take another string as input and return the result of multiplying the two strings together.
-function stringMultiply(string1, string2) {
-  let multi = BigInt(string1) * BigInt(string2);
-  return multi.toString();
+
+function homeworkMulti(string1, string2) {
+  const num1 = string1.split("").map(Number);
+  const num2 = string2.split("").map(Number);
+  const len1 = num1.length;
+  const len2 = num2.length;
+
+  const result = new Array(len1 + len2).fill(0);
+
+  for (let i = len1 - 1; i >= 0; i--) {
+    for (let j = len2 - 1; j >= 0; j--) {
+      const product = num1[i] * num2[j];
+      const sum = product + result[i + j + 1];
+      result[i + j] += (sum / 10) | 0;
+      result[i + j + 1] = sum % 10;
+    }
+  }
+  return result.join("").replace(/^0+/, "") || "0";
 }
-let multiply = stringMultiply(string1, string2);
-console.log(string1 + " * " + string2 + " = " + multiply);
+
+const plus = homeworkPlusMinus(str1, str2, stringPlus);
+console.log(`${str1} + ${str2} = ${plus}`);
+
+const minus = homeworkPlusMinus(str1, str2, stringMinus);
+console.log(`${str1} - ${str2} = ${minus}`);
+
+const divide = homeworkDivide(str1, str2);
+console.log(`${str1} / ${str2} = ${divide}`);
+
+const multiply = homeworkMulti(str1, str2);
+console.log(`${str1} * ${str2} = ${multiply}`);
