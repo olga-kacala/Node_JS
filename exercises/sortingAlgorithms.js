63aef3ce09b4aa6f3124a33c1797b1f8936c3f73
olga-kacala <olga.kacala@gmail.com>
2024-10-25 16:27:21 +0200

    merge sort

--- exercises/sortingAlgorithms.js
+++ exercises/sortingAlgorithms.js
@@ -13,7 +13,7 @@ function bubbleSort(arr) {
     }
   } while (sorted);
 }
-const arr = [88, 20, -2, 4, -6];
+const arr = [88, 20, -9, 4, -6, 9];
 // bubbleSort(arr);
 // console.log(arr)
 
@@ -34,4 +34,41 @@ function insertion(arr) {
 // insertion(arr);
 // console.log(arr);
 
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
 
+// console.log(quickSort(arr))
+
+function mergeSort(arr) {
+  if(arr.length <2) {return arr}
+  let mid = Math.floor(arr.length/2);
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid);
+return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge (left, right) {
+  let sorted = [];
+  while (left.length && right.length){
+    if(left[0]< right[0]){
+      sorted.push(left.shift())
+    } else {sorted.push(right.shift())}
+  }
+  return [...sorted, ...left, ...right]
+}
+
+console.log(mergeSort(arr));
